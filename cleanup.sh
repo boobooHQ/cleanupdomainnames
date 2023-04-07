61089d8e4665fe31291c71fddec40e63cbec9f0b
themoonbaba <64802126+themoonbaba@users.noreply.github.com>
2023-04-07 11:28:56 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,8 @@
 
 # read the input from stdin
 while read line; do
+  # remove commas from the line
+  line=$(echo "$line" | tr -d ',')
   # remove all the digits that occur before the first period (.)
   output=$(echo "$line" | sed 's/^[0-9]*//')
   # print the modified line
