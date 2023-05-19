bb19bf582e0a74d012f2d8269acfb9a3d3dc8e44
boobooHQ <64802126+themoonbaba@users.noreply.github.com>
2023-05-19 12:55:23 +0530

    Update reverseall.sh

--- reverseall.sh
+++ reverseall.sh
@@ -1,8 +1,3 @@
-
-
-If the list is stored in a file, with one word per line, you can use the following script to reverse the positions of all the letters in each word:
-
-
 #!/bin/bash
 
 # Read the list from the file
@@ -11,11 +6,3 @@ do
     # Reverse the element and print it
     echo "$(echo "$element" | rev)"
 done < "$1"
-
-
-To use this script, save it to a file and make it executable with the chmod command. Then, you can run it and pass the name of the file as an argument. For example:
-
->>  ./reverse-list.sh list.txt
-
-
-This will read the list from list.txt, reverse the positions of all the letters in each word, and print the result to the terminal.
